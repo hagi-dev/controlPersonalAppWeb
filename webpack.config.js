b3e58cbc2c77259d2fb98ab2079d8991bdc28a40
hagi torres <hagiraitorresmacedo@gmail.com>
2021-11-03 22:15:16 -0500

    iniciando proyecto de control de personal

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    }, 
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ]
+            },
+            {
+                test: /\.(png|gif|jpg|svg|pdf)$/,
+                use:[
+                    {
+                        'loader':'file-loader',
+                        options: {
+                            name:'assets/[hash].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            filename: 'index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
+    devServer:{
+        port: 4000,
+    }
+}
