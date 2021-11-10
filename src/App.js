7e714c159d3f203372eb056aff056f21e503e49f
Jalz DeLe'Z <j4lz.dele@outlook.es>
2021-11-10 12:11:25 -0500

    comboBox

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { Children } from 'react';
 import Menu from './components/Menu';
 import './assets/Styles/App.scss';
 import Tabla from './components/Tabla';
-import {ComboBox_C} from './components/ComboBox.jsx'
+import ComboBox_C from './components/ComboBox.jsx'
+  
+/* 
+  function TodoList_C(props){
+      return(
+          <section>
+              <ol>
+                {props.children}
+              </ol>
+          </section>
+      )
+  }
+
+  function TodoItem_C(props){
+      return(
+          <li>
+              <p>{props.text}</p>
+          </li>
+      )
+  } */
+
+  
+const todoList = [
+    { text: 'Cortar Cebolla', id:'500'},
+    { text: 'Tomar el Cuso de Intro con React',id:'1'},
+    { text: 'Lorem Ipsus', id:'2'},
+    { text: 'Miroquezada Rha', id:'3'}
+  ]
 
 const App = () => {
 
     return (
         <React.Fragment>
 
-            <ComboBox_C/>
+            <ComboBox_C
+                text = {"HAGI"}
+                todoList = {todoList}
+                width= {120}
+            
+            />
+
+{/*             <TodoList_C>
+
+                {todoList.map(fAux =>(
+                     <TodoItem_C key={fAux.text} text= {fAux.text }/>
+                ))}
+            
+            </TodoList_C> */}
 
         </React.Fragment>
     )
--- src/components/ComboBox.jsx
+++ src/components/ComboBox.jsx
@@ -1,88 +1,81 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
+import MenuItem from '@material-ui/core/MenuItem'; 
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
-const useStyles = makeStyles((theme) => ({
+
+
+function ComboBox_C (props) {
+
   
-  select: {
-    fontFamily: "mulish",
-
-    color: "#666666",
-    '&:before': {
-        borderColor: "#2EA39D",
+  const useStyles = makeStyles((theme) => ({
+    select: {
+      fontFamily  : "mulish",
+      color       : "#666666",
+      '&:before'  : {borderColor: "#2EA39D"},
+      '&:after'   : {borderColor: "#666666",
+      }
     },
-    '&:after': {
-        borderColor: "#666666",
-    }
-  },
-  icon: {
-    
-    fill: "#2EA39D", 
-  },
+    icon: {
+      fill: "#2EA39D", 
+    },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: props.width,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
 
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-}));
-
-function ComboBox_C () {
   const classes = useStyles();
-  const [age, setAge] = React.useState('');
+  const [todoList, setTodoList] = React.useState('');
 
   const handleChange = (event) => {
-    setAge(event.target.value);
+    setTodoList(event.target.value);
+    console.log(event.target.value);
   };
 
-  return (
-    <div>
-      <FormControl  
-        className={classes.formControl}>
-        
-        <InputLabel
-            style = {{color : "#2EA39D", 
-            fontFamily: "mulish",}}
+  return ( 
+    <FormControl  
+      className={classes.formControl}>
+      
+      <InputLabel 
+          style = {{color : "#2EA39D", 
+          fontFamily: "mulish",}}
+          fullWidth 
+          id="demo-simple-select-label">{props.text}</InputLabel>
+      <Select         
 
-            fullWidth 
-            id="demo-simple-select-label"
-            
-            >Hagi Rai</InputLabel>
-        <Select         
+          value="1"
+          className={classes.select}
+          inputProps={{
+              classes: {
+                  icon: classes.icon,
+              },
+          }}
 
-            value="1"
-            className={classes.select}
-            inputProps={{
-                classes: {
-                    icon: classes.icon,
-                },
-            }}
+          fullWidth
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={todoList}
+          onChange={handleChange}
+      >
+        {props.todoList.map(fAux =>(
 
-        
+          <MenuItem key={fAux.id} value={fAux.id}>{fAux.text }</MenuItem>
 
-            fullWidth
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={age}
-            onChange={handleChange}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-          <MenuItem value={30}>aaA</MenuItem>
-        </Select>
-      </FormControl>
-    </div>
+        ))}
+        {/* <MenuItem value={20}>Twenty</MenuItem>  */}
+
+      </Select>
+    </FormControl> 
   );
 }
 
-export {ComboBox_C};
+export default ComboBox_C;
 
 /*import * as React from 'react';
 
