1c167ee005ae71c56b15ab67ac75df3538abd8cb
hagi torres <hagiraitorresmacedo@gmail.com>
2021-12-08 17:18:39 -0500

    regresando para pruena

---
+++ src/containers/RegistrarHuella.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import TablaHuella from '../components/TablaHuellas';
+import {TextField,MenuItem} from '@material-ui/core';
+import Menu from '../components/Menu';
+import Perfil from '../components/Usuario';
+import '../assets/styles/components/registrarHuella.scss';
+import '../assets/Styles/components/Buton.scss';
+
+
+const RegistrarHuella = () => {
+    return (
+        <div className="RegistrarHuella"> 
+             <div className="RegistrarHuella__menu">
+                <Menu/> 
+            </div>
+            <form className="RegistrarHuella__cuerpo" style={{backgroundColor:"white",width:"100%",height:"100%"}}>
+                <div className="RegistrarHuella__cuerpo-perfil">
+                 <Perfil/>
+                </div>
+                <div className="RegistrarHuella__cuerpo-titulo">
+                    <h1>Personal</h1>
+                </div>
+                <div className="RegistrarHuella__cuerpo-contenido">
+                    <div className="RegistrarHuella__registrar">
+                        <div className="RegistrarHuella__registrar-titulo"><h2>Registrar Huella</h2></div>
+                                                    
+                    </div>
+                    <div className="RegistrarHuella__tabla">
+                        <div className="RegistrarHuella__fila">
+                        <TextField
+                                    id="outlined-select-currency"
+                                    select
+                                    label="Area"
+                                    value={''}
+                                    required
+                                    style={{width:"60%"}}
+                                    helperText=""
+                                    name="area"
+                                    onChange={getSelection}
+                                    >
+                                </TextField>    
+                        </div>
+                        <TablaHuella/>
+                    </div>
+                </div>
+                <div className="RegistrarHuella__botones">
+                    <button className='Button' style={{width:'30%'}}>Guardar</button> 
+                    <button className='Button'style={{width:'30%'}}>Cancelar</button>
+                </div>
+                <div className="RegistrarHuella__Cerrar"><ion-icon  name="close-circle-outline"></ion-icon></div>
+            </form>
+        </div>
+    )
+}
+
+export default RegistrarHuella;
--- src/containers/RegistrarPersonal.js
+++ src/containers/RegistrarPersonal.js
@@ -1,82 +1,27 @@
-import React,{useState} from 'react' ;
+import React, {useState,useEffect,forwardRef} from 'react';
 import '../assets/Styles/components/RegistrarPersonal.scss';
-import ComboBox from '../components/ComboBox';
-import IconButton from '@material-ui/core/IconButton';
-import Boton from '../components/Buton';
-import Inputs from '../components/Inputs';
+import '../assets/Styles/components/Buton.scss';
 import SearchIcon from '@material-ui/icons/Search';
 import TablaHuella from '../components/TablaHuellas';
 import Menu from '../components/Menu';
+import {TextField,MenuItem} from '@material-ui/core';
 import Perfil from '../components/Usuario';
 
 const RegistrarPersonal = () => {
-    const configInput = {
-        ancho: '95%',
-        title: 'DNI',
-        type: 'text',
-        name: 'Dni',
-        paddingLetf:"0%",
-        paddingRight:"0%",
-    }
-    const configInput2 = {
-        ancho: '100%',
-        title: 'Nombre',
-        type: 'text',
-        name: 'Nombre',
-        paddingLetf:"9%",
-        paddingRight:"0%",
-    }
-    const configInput3 = {
-        ancho: '100%',
-        title: 'Apellidos',
-        type: 'text',
-        name: 'apellidos',
-        paddingLetf:"12%",
-        paddingRight:"0%",
-    }
-    const configInput4 = {
-        ancho: '95%',
-        title: 'Fecha de Nacimiento',
-        type: 'date',
-        name: 'fechaNacimiento',
-        paddingLetf:"0%",
-        paddingRight:"0%",
-    }
-    const configInput5 = {
-        ancho: '100%',
-        title: 'Subir Foto',
-        border:"0px",
-        padding:"0",
-        name: 'foto',
-        type: 'file',
-        paddingLetf:"9%",
-        paddingRight:"0%",
-    }
-    const configInput6 = {
-        ancho: '46%',
-        title: 'Telefono',
-        type: 'number',
-        name: 'telefono',
-    }
-    const configButon = {
-        title: 'Guardar',
-        ancho: '45%',
-        marginTop:'50px',
-        id:'contrato__guardar'
-    }
-    const configButon3 = {
-        title: 'Cancelar',
-        ancho: '45%',
-        marginTop:'50px',
-        id:'contrato__cancelar'
-    }
-    const configButon2 = {
-        title: 'Agregar',
-        ancho: '30%',
-        marginTop:'0px',
-        alto: '30px',
-        id:'contrato__agregar'
-    }
+    const [filtro, setFiltro] = React.useState(0);
+    const [getData,setData] = useState({
+        id: '',
+        dni:'',
+        nombre:'',
+        paterno:'',
+        materno:'',
+        sexo:'',
+        fechaNacimiento:'',
+        telefono:'',
+        foto:'',
+        
+    });
+    const [getDataHuella,setData2] = useState([]);
     const todoList = [
         { text: '10:00 AM', id:'500'},
         { text: '10:00 AM',id:'1'},
@@ -85,15 +30,15 @@ const RegistrarPersonal = () => {
       ]
 
 	return (
-		<div className="RegistrarContrato">
-             <div className="RegistrarContrato__menu">
+		<div className="RegistrarPersonal">
+             <div className="RegistrarPersonal__menu">
                 <Menu/> 
             </div>
-            <form className="RegistrarContrato__cuerpo" style={{backgroundColor:"white",width:"100%",height:"100%"}}>
-                <div className="RegistrarContrato__cuerpo-perfil">
+            <form className="RegistrarPersonal__cuerpo" style={{backgroundColor:"white",width:"100%",height:"100%"}}>
+                <div className="RegistrarPersonal__cuerpo-perfil">
                  <Perfil/>
                 </div>
-                <div className="RegistrarContrato__cuerpo-titulo">
+                <div className="RegistrarPersonal__cuerpo-titulo">
                     <h1>Personal</h1>
                 </div>
                 <div className="RegistrarPersonal__cuerpo-contenido">
@@ -101,53 +46,120 @@ const RegistrarPersonal = () => {
                         <div className="RegistrarPersonal__registrar-titulo"><h2>Registrar Personal</h2></div>
                             <div className="RegistrarPersonal__Combos">
                                 <div className="RegistrarPersonal__fila1">
-                                    <Inputs
-                                        configInput={configInput}
-                                    />
-                                    <Inputs
-                                        configInput={configInput2}
-                                    />
+                                    <TextField
+                                        id="outlined-select-currency"
+                                        label="Dni"
+                                        value={''}
+                                        required
+                                        style={{width:"40%"}}
+                                        helperText=""
+                                        name="dni"
+                                        onChange={getSelection}
+                                        >
+                                    </TextField>   
+                                    <TextField
+                                        id="outlined-select-currency"
+                                        select
+                                        label="Area"
+                                        value={''}
+                                        required
+                                        style={{width:"60%"}}
+                                        helperText=""
+                                        name="area"
+                                        onChange={getSelection}
+                                        >
+                                    </TextField>
                                 </div> 
                                 <div className="RegistrarPersonal__fila2">
-                                    <ComboBox
-                                        text = {"Genero"}
-                                        todoList = {todoList}
-                                        width = {'45%'}
-                                    />    
-                                    <Inputs
-                                        configInput={configInput3}
-                                    /> 
+                                    <TextField
+                                            id="outlined-select-currency"
+                                            select
+                                            label="Area"
+                                            value={''}
+                                            required
+                                            style={{width:"60%"}}
+                                            helperText=""
+                                            name="area"
+                                            onChange={getSelection}
+                                            >
+                                        </TextField>   
+                                        <TextField
+                                            id="outlined-select-currency"
+                                            select
+                                            label="Area"
+                                            value={''}
+                                            required
+                                            style={{width:"60%"}}
+                                            helperText=""
+                                            name="area"
+                                            onChange={getSelection}
+                                            >
+                                        </TextField> 
                                 </div>  
                                 <div className="RegistrarPersonal__fila2">
-                                    <Inputs
-                                            configInput={configInput4}
-                                    />
-                                    <Inputs
-                                        configInput={configInput5}
-                                    />
+                                    <TextField
+                                        id="outlined-select-currency"
+                                        select
+                                        label="Area"
+                                        value={''}
+                                        required
+                                        style={{width:"60%"}}
+                                        helperText=""
+                                        name="area"
+                                        onChange={getSelection}
+                                        >
+                                    </TextField>   
+                                    <TextField
+                                        id="outlined-select-currency"
+                                        select
+                                        label="Area"
+                                        value={''}
+                                        required
+                                        style={{width:"60%"}}
+                                        helperText=""
+                                        name="area"
+                                        onChange={getSelection}
+                                        >
+                                    </TextField>
                                 </div> 
                                 <div className="RegistrarPersonal__fila2">
-                                    <Inputs
-                                                configInput={configInput6}
-                                        />
+                                    <TextField
+                                        id="outlined-select-currency"
+                                        select
+                                        label="Area"
+                                        value={''}
+                                        required
+                                        style={{width:"60%"}}
+                                        helperText=""
+                                        name="area"
+                                        onChange={getSelection}
+                                        >
+                                    </TextField>
                                 </div>                               
                             </div>                        
                     </div>
                     <div className="RegistrarPersonal__tabla">
                         <div className="RegistrarPersonal__fila">
-                            <ComboBox
-                                text = {"Horario"}
-                                todoList = {todoList}
-                                width = {'60%'}
-                            />   
-                            <Boton configButon={configButon2}/>   
+                            <TextField
+                                id="outlined-select-currency"
+                                select
+                                label="Area"
+                                value={''}
+                                required
+                                style={{width:"60%"}}
+                                helperText=""
+                                name="area"
+                                onChange={getSelection}
+                                >
+                            </TextField>   
+                            <button className="button" style={{width:"30%"}}><h5>Agregar</h5></button>   
                         </div>
                         <TablaHuella/>
                     </div>
                 </div>
                 <div className="RegistrarPersonal__botones">
-                    <Boton configButon={configButon}/> 
-                    <Boton configButon={configButon3}/> 
+                    <button className="button" style={{width:"40%"}}><h5>Guardar</h5></button>
+                    <button className="button" style={{width:"40%"}}><h5>Cancelar</h5></button>
                 </div>
                 <div className="RegistrarPersonal__Cerrar"><ion-icon  name="close-circle-outline"></ion-icon></div>
             </form>
